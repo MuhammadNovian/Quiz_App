6f8698bb4da95b0b8c59ca5c4e0dc23c41f76ddd
MuhammadNovian <90599988+MuhammadNovian@users.noreply.github.com>
2025-04-27 18:56:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,79 @@
 // Daftar Soal
 const questions = [
   {
-    question: "What is the capital of France?",
-    answers: ["Berlin", "Madrid", "Paris", "Rome"],
+    question: "1. Siapa penemu listrik?",
+    answers: ["Thomas Edison", "Albert Einstein", "Karl Benz", "Benjamin Franklin"],
+    correct: 3,
+  },
+  {
+    question: "2. Apa unsur paling ringan di alam semesta?",
+    answers: ["Helium", "Hidrogen", "Oksigen", "Karbon"],
+    correct: 1,
+  },
+  {
+    question: "3. Apa nama planet yang dikenal dengan 'Red Planet'?",
+    answers: ["Mars", "Venus", "Jupiter", "Saturnus"],
+    correct: 0,
+  },
+  {
+    question: "4. Apa ibu kota Prancis?",
+    answers: ["Berlin", "Madrid", "Paris", "Roma"],
     correct: 2,
   },
   {
-    question: "Which planet is known as the Red Planet?",
-    answers: ["Earth", "Mars", "Jupiter", "Saturn"],
+    question: "5. Siapa penulis buku 'Harry Potter'?",
+    answers: ["J.R.R. Tolkien", "J.K. Rowling", "George R.R. Martin", "Suzanne Collins"],
     correct: 1,
   },
   {
-    question: "What is the largest mammal?",
-    answers: ["Elephant", "Whale", "Shark", "Rhino"],
+    question: "6. Satuan internasional untuk panjang adalah?",
+    answers: ["Kilogram", "Liter", "Meter", "Watt"],
+    correct: 2,
+  },
+  {
+    question: "7. Benua mana yang memiliki jumlah negara terbanyak?",
+    answers: ["Asia", "Afrika", "Eropa", "Amerika"],
     correct: 1,
+  },
+  {
+    question: "8. Siapa yang pertama kali mengelilingi dunia?",
+    answers: ["Christopher Columbus", "Ferdinand Magellan", "Vasco da Gama", "Marco Polo"],
+    correct: 1,
+  },
+   {
+    question: "9. Apa nama planet terbesar di tata surya kita?",
+    answers: ["Mars", "Bumi", "Jupiter", "Uranus"],
+    correct: 2,
+  },
+  {
+    question: "10. Di negara mana Taj Mahal berada?",
+    answers: ["Pakistan", "Bangladesh", "India", "Nepal"],
+    correct: 2,
+  },
+  {
+    question: "11. Siapa yang menemukan teori relativitas?",
+    answers: ["Isaac Newton", "Albert Einstein", "Nikola Tesla", "Galileo Galilei"],
+    correct: 1,
+  },
+  {
+    question: "12. Apa nama laut terbesar di dunia?",
+    answers: ["Laut Kaspia", "Laut Mediterania", "Laut Karibia", "Laut Selatan"],
+    correct: 0,
+  },
+  {
+    question: "13. Mata uang yang digunakan di Jepang adalah?",
+    answers: ["Dollar", "Won", "Yen", "Peso"],
+    correct: 2,
+  },
+  {
+    question: "14. Siapa yang pertama kali menciptakan mesin uap?",
+    answers: ["Thomas Edison", "James Watt", "Nikola Tesla", "Henry Ford"],
+    correct: 1,
+  },
+  {
+    question: "15. Di bawah ini, yang mana yang merupakan gas yang paling banyak di atmosfer Bumi?",
+    answers: ["Oksigen", "Karbon dioksida", "Nitrogen", "Hidrogen"],
+    correct: 2,
   },
 ];
 
